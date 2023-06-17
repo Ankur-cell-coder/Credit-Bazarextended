994a90796dd1c9d241c464255432f8b886de8596
Ankur-cell-coder <ankurbgs6243@gmail.com>
2023-06-17 06:29:14 +0530

    css fixed for each page

--- src/components/finance/FinanceInitate.js
+++ src/components/finance/FinanceInitate.js
@@ -13,23 +13,23 @@ function FinanceInitate() {
   return (
     <Finance1>
       <Navbarfinancer/>
-      <div className="requestdetail">
-        <h2>Request Details</h2>
-        <h3>Request Id</h3>
-        <div>Offered Amount: INR 7,50,000</div>
-        <div>Margin (25%): INR 2,50,000</div>
-        <div>Interest (12%): INR 22,191.78</div>
-        <div>Term: 90 days</div>
-      </div>
-
-      <div>
-        <h1>Make An Offer</h1>
-
-        <label htmlFor="amount" style={{ marginLeft: "-90px" }}>
-          Amount:
-        </label>
-        <br />
-        <input
+      
+    <div className="content">
+        <div className="offer-details">
+            <h2>Request details</h2>
+            <div className="title">Request ID</div>
+            <div className="details">
+                <p>Offered Amount: INR 7,50,000</p>
+                <p>Margin (25%): INR 2,50,000</p>
+                <p>Interest (12%): INR 22,191.78</p>
+                <p>Term: 90 days</p>
+            </div>
+        </div>
+        <div className="offer-section">
+            <h2>Make an Offer</h2>
+            <div className="form-group">
+                <label for="amount">Amount:</label>
+                <input
           type="txt"
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
@@ -38,13 +38,10 @@ function FinanceInitate() {
           placeholder="Enter Amount"
           required
         />
-        <br />
-
-        <label htmlFor="margin" style={{ marginLeft: "-90px" }}>
-         Margin:
-        </label>
-        <br />
-        <input
+        </div>
+            <div className="form-group">
+                <label for="Margin">Margin:</label>
+               <input
           type="txt"
           value={margin}
           onChange={(e) => setMargin(e.target.value)}
@@ -53,13 +50,10 @@ function FinanceInitate() {
           placeholder="Enter Margin"
           required
         />
-        <br />
-
-        <label htmlFor="interest" style={{ marginLeft: "-90px" }}>
-          Interest:
-        </label>
-        <br />
-        <input
+        </div>
+            <div className="form-group">
+                <label for="interest">Interest Rate:</label>
+                  <input
           type="txt"
           value={interest}
           onChange={(e) => setInterest(e.target.value)}
@@ -68,13 +62,10 @@ function FinanceInitate() {
           placeholder="Enter Interest Rate"
           required
         />
-        <br />
-
-        <label htmlFor="terms" style={{ marginLeft: "-90px" }}>
-         Terms (in days):
-        </label>
-        <br />
-        <input
+        </div>
+            <div className="form-group">
+                <label for="term">Term (in days):</label>
+               <input
           type="txt"
           value={terms}
           onChange={(e) => setTerms(e.target.value)}
@@ -83,31 +74,36 @@ function FinanceInitate() {
           placeholder="Enter Term"
           required
         />
-        <br />
-      </div>
-     
-     <div className="proscons">
-        <div className="pros">
-           <h1>Pros:</h1>
-         <div>  Low interest rate compared to market average.</div>
-         <div>Short term with quick return on investment.</div>
-         <div>No collateral requirement.</div>
+                           </div>
         </div>
-        <div className="cons">
-        <h1>Cons:</h1>
-         <div>Higher monthly payments due to shorter term.</div>
-         <div>Opportunity cost of tying up funds in this investment.</div>
+        <div className="pros-cons">
+            <div className="pros">
+                <h3>Pros:</h3>
+                <ul>
+                    <li>Low interest rate compared to market average.</li>
+                    <li>Short term with quick return on investment.</li>
+                    <li>No collateral requirement.</li>
+                </ul>
+            </div>
+            <div className="cons">
+                <h3>Cons:</h3>
+                <ul>
+                    <li>Higher monthly payments due to shorter term.</li>
+                    <li>Opportunity cost of tying up funds in this investment.</li>
+                </ul>
+            </div>
         </div>
-     </div>
-
-     <div className="buttonsection">
-        <button className="button" onClick={()=>{navigate('/financeofferacknowledgement')}}>
+        <div className="btn-container">
+           <button className="btn" onClick={()=>{navigate('/financeofferacknowledgement')}}>
             Place an Offer
         </button>
-        <button className="button" onClick={()=>{navigate('/financesdashboard')}}>
+        <button className="btn" onClick={()=>{navigate('/financesdashboard')}}>
             Decline Request
         </button>
-     </div>
+        </div>
+    </div>
+
+
 
     </Finance1>
   );
@@ -117,6 +113,85 @@ export default FinanceInitate;
 
 const Finance1 = styled.div`
 
+
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        .content {
+            margin-top: 50px; /* Adjust the margin to move content down */
+            padding: 20px;
+        }
+        .details {
+            margin-bottom: 50px;
+        }
+        .offer-details {
+            background-color: #F8F9FA;
+            padding: 20px;
+            margin-bottom: 20px;
+        }
+        .offer-details h2 {
+            margin-top: 0;
+        }
+        .offer-details .title {
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+        .offer-details .details {
+            color: #666;
+        }
+        .pros-cons {
+            display: flex;
+            justify-content: space-between;
+            margin-top: 0px;
+        }
+        .pros-cons h3 {
+            margin: 0px;
+        }
+        .pros {
+            color: green;
+        }
+        .cons {
+            color: red;
+        }
+        .btn-container {
+            display: flex;
+            justify-content: center;
+            margin-top: 30px;
+        }
+        .btn {
+            color: white;
+            background-color: orange;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            display: inline-block;
+            margin-right: 10px;
+        }
+        .btn:hover {
+            background-color: #0056b3;
+        }
+        .form-group {
+            margin-bottom: 20px;
+        }
+        .form-group label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        .form-group input {
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            width: 100%;
+        }
+        .form-group select {
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            width: 100%;
+        }
+    
 
 
 `;
--- src/components/finance/FinanceOfferAcknowledgement.js
+++ src/components/finance/FinanceOfferAcknowledgement.js
@@ -8,30 +8,67 @@ function FinanceOfferAcknowledgement() {
   return (
     <Finance1>
       <Navbarfinancer/>
-      <h1>Place an offer Acknowledgement</h1>
-      <div>
-        Your Request for Finance has been successfully placed an offer! See the
-        details below.
-      </div>
-
-      <div className="content">
-        <div> Lender Name: Suresh</div>
-        <div>Offered Amount: INR 7,50,000</div>
-        <div>Margin (25%): INR 2,50,000</div>
-        <div>Interest (12%): INR 22,191.78</div>
-        <div>Term: 90 days</div>
-      </div>
-
-      <div className="notify">
-          <div>We will notify you soon, Thank you for using our platform!</div>
-          <button className="button" onClick={()=>{navigate('/financesdashboard')}}>
+     
+    <div id="container">
+        <div className="header">
+            <h1>Place an offer Acknowledgement</h1>
+            <p>Your Request for Finance has been successfully placed an offer! See the details below.</p>
+        </div>
+        <div className="content">
+            <p>Lender Name: Suresh</p>
+            <p>Offered Amount: INR 7,50,000</p>
+            <p>Margin (25%): INR 2,50,000</p>
+            <p>Interest (12%): INR 22,191.78</p>
+            <p>Term: 90 days</p>
+        </div>
+        <div className="content center">
+            <p>We will notify you soon, Thank you for using our platform!</p>
+            <button className="btn" onClick={()=>{navigate('/financesdashboard')}}>
             OK
           </button>
-      </div>
+        </div>
+    </div>
+
+
+
+     
     </Finance1>
   );
 }
 
 export default FinanceOfferAcknowledgement;
 
-const Finance1 = styled.div``;
+const Finance1 = styled.div`
+
+body {
+  font-family: Arial, sans-serif;
+}
+#container {
+  width: 80%;
+  margin: auto;
+}
+.header {
+  text-align: center;
+  padding: 20px;
+  color: #333;
+}
+.content {
+  background-color: #F8F9FA;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+.btn {
+  color: white;
+  background-color: orange;
+  padding: 10px 40px;
+  text-decoration: none;
+  border-radius: 5px;
+  display: inline-block;
+}
+.btn:hover {
+  background-color: #0056b3;
+}
+.center {
+  text-align: center;
+}
+`;
